1d8df3d7b41d96c99b5a31ed5fe6cb7935563d5d
= <computergeek3.142@gmail.com>
2021-05-25 20:08:28 -0700

    Finishing website (sort of)

--- docs/generate.py
+++ docs/generate.py
@@ -2,6 +2,40 @@ from yattag import Doc
 import os
 import re
 
+search_script = """
+<input id="search" onchange="update_search(this)"></input>
+<script type="text/javascript">
+function update_search(dom){
+	search_value = dom.value;
+	search_results = [];
+	for(var i = 0; i < tags.length; i++){
+		if(tags[i][0].indexOf(search_value) === 0){
+			search_results.push(tags[i]);
+		}
+	}
+	for(i = 0; i < search_results.length && i <= 5; i++){
+		console.log(tags[i]);
+	}
+}
+"""
+
+popup_script = """
+<div id="popup" style="display: none; position: absolute; padding: 2px; border: 2px solid black; border-radius: 2px; background: white;"></div>
+<script type="text/javascript">
+popup = document.getElementById("popup");
+function popup_window(dom, text){
+	rect = dom.getBoundingClientRect()
+	popup.innerHTML = text;
+	popup.style.top = rect.bottom + window.scrollY + 5 + "px";
+	popup.style.left = rect.left + window.scrollX + "px";
+	popup.style.display = "inline-block";
+}
+function close_popup(){
+	popup.style.display = "none";
+}
+</script>
+"""
+
 results_index = []
 
 def index_file(lines, name):
@@ -19,6 +53,7 @@ def index_file(lines, name):
 			while definition_line_num and lines[definition_line_num][:2] == "//":
 				definition_description = lines[definition_line_num][2:] + definition_description
 				definition_line_num -= 1
+			definition_description = definition_description.replace("\n", "<br>")
 			results.append(("definition", definition_name, definition_description, line, name))
 		if line[:6] == "axiom ":
 			axiom_name = line.split(":")[0].split()[1]
@@ -29,6 +64,7 @@ def index_file(lines, name):
 			while axiom_line_num and lines[axiom_line_num][:2] == "//":
 				axiom_description = lines[axiom_line_num][2:] + axiom_description
 				axiom_line_num -= 1
+			axiom_description = axiom_description.replace("\n", "<br>")
 			results.append(("axiom", axiom_name, axiom_description, line, name))
 		if line[:6] == "prove ":
 			proof_name = line.split(":")[0].split()[1]
@@ -47,6 +83,7 @@ def index_file(lines, name):
 				line = next(lines_iter, None)
 				line_num += 1
 			proof_code += "}"
+			proof_description = proof_description.replace("\n", "<br>")
 			results.append(("proof", proof_name, proof_description, proof_code, name))
 		line = next(lines_iter, None)
 		line_num += 1
@@ -86,11 +123,15 @@ def source_replace(match, replacement):
 		return match.group()[0] + replacement + match.group()[-1]
 	else:
 		return replacement
+	
 
-def highlight_source(text):
+def highlight_source(text, relative_head):
+	text = " " + text + " "
 	for replacement in source_replacements:
 		text = re.sub(replacement[0], lambda match: source_replace(match, replacement[1]), text)
-	return text
+	for result in results_index:
+		text = re.sub(r"\W" + result[1] + r"\W", lambda match: source_replace(match, "<b style=\"cursor: pointer;\" onclick=\"window.location.href = '" + relative_head + result[4] + "/" + result[1] + ".html';\" onmouseover=\"popup_window(this, '" + result[1] + " | " + result[4] + "');\" onmouseout=\"close_popup();\">" + result[1] + "</b>"), text)
+	return text[1:-1]
 
 def write_results(results, name):
 	os.chdir("../docs")
@@ -104,16 +145,26 @@ def write_results(results, name):
 			with tag("html"):
 				with tag("head"):
 					with tag("title"):
-						text("result")
+						text("CORE result")
+					with tag("link", rel = "stylesheet", href="../style.css"):
+						pass
+					with tag("link", rel = "stylesheet", href="https://fontlibrary.org/face/pixelated", type="text/css"):
+						pass
 				with tag("body"):
-					with tag("div", klass = "result_type"):
-						text(result[0])
-					with tag("div", klass = "result_name"):
-						text(result[1])
-					with tag("div", klass = "result_description"):
-						text("Description: " + result[2])
-					with tag("code", style="white-space: pre-wrap;"):
-						doc.asis(highlight_source(result[3]))
+					with tag("h1", style = "font-size: 4vw"):
+						text("CORE library")
+					with tag("div", klass = "page_body"):
+						with tag("div", klass = "card"):
+							doc.asis("<b>Result type</b>: " + result[0])
+							doc.asis("<br><b>Result name</b>: " + result[1])
+							if(result[2] == ""):
+								doc.asis("<br><b>Description</b>: None given")
+							else:
+								doc.asis("<br><b>Description</b>: " + result[2] + "")
+							doc.asis("<br><b>Source</b>:<br><br>")
+							with tag("code", style="white-space: pre-wrap; cursor: default;"):
+								doc.asis(highlight_source(result[3], "../"))
+				doc.asis(popup_script)
 			file_handle.write(doc.getvalue())
 	os.chdir("../../proofs")
 
@@ -121,30 +172,6 @@ def write_results(results, name):
 os.chdir("../proofs");
 files = [f for f in os.listdir(".") if os.path.isfile(f)]
 files.sort()
-
-#Generate index.html
-doc, tag, text = Doc().tagtext()
-doc.asis("<!DOCTYPE html>")
-with tag("html"):
-	with tag("head"):
-		with tag("title"):
-			text("CORE home")
-	with tag("body"):
-		with tag("div", klass = "header"):
-			text("Files")
-		with tag("div", klass = "files"):
-			for file in files:
-				parts = file.split(".")
-				parts.pop()
-				name = ".".join(parts) + ".html"
-				with tag("a", href = "proofs/" + name):
-					text(file)
-				with tag("br"):
-					pass
-os.chdir("../docs")
-with open("index.html", "w") as index:
-	index.write(doc.getvalue())
-os.chdir("../proofs")
 
 #Parse through each proof file
 for file in files:
@@ -155,24 +182,36 @@ for file in files:
 	parts.pop()
 	name = ".".join(parts)
 	results = index_file(file_lines, name)
-	write_results(results, name)
 	results_index.extend(results)
+	write_results(results, name)
 	name = name + ".html"
-	file_text = highlight_source(file_text)
+	file_text = highlight_source(file_text, "../")
 	doc, tag, text = Doc().tagtext()
-	doc.asis("<!DOCTYPE html>")
-	with tag("html"):
-		with tag("head"):
-			with tag("title"):
-				text("file")
-		with tag("body"):
-			with tag("div", klass = "header"):
-				text("CORE Source")
-			with tag("div", klass = "CORE_source"):
-				with tag("code", style="white-space: pre-wrap;"):
-					doc.asis(file_text)
-	os.chdir("../docs")
-	with open("proofs/" + name, "w") as file_handle:
-		file_handle.write(doc.getvalue())
-	os.chdir("../proofs")
 
+#Generate index.html
+doc, tag, text = Doc().tagtext()
+doc.asis("<!DOCTYPE html>")
+with tag("html"):
+	with tag("head"):
+		with tag("title"):
+			text("CORE home")
+		with tag("link", rel = "stylesheet", href="style.css"):
+			pass
+		with tag("link", rel = "stylesheet", href="https://fontlibrary.org/face/pixelated", type="text/css"):
+			pass
+	with tag("body"):
+		with tag("h1", style = "font-size: 4vw"):
+			text("CORE library")
+		with tag("div", klass = "page_body"):
+			with tag("div", klass = "card"):
+				text("Click on each result to see its proof")
+			with tag("div", klass = "card"):
+				for result in results_index:
+					with tag("a", href = result[4] + "/" + result[1] + ".html"):
+						text(result[4] + ": " + result[1])
+					with tag("br"):
+						pass
+os.chdir("../docs")
+with open("index.html", "w") as index:
+	index.write(doc.getvalue())
+os.chdir("../proofs")
